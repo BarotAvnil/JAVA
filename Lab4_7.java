f7546f4ab3995b60ecf6ba71832f9aa0337856c3
BarotAvnil <avnilbarot236@gmail.com>
2024-04-09 16:18:43 +0530

    Hello

--- Lab4_7.java
+++ Lab4_7.java
@@ -2,10 +2,10 @@ import java.util.Scanner;
 
 public class Lab4_7 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter a string: ");
-        String input = scanner.nextLine();
+        String input = sc.nextLine();
 
         for (int i=0; i<input.length(); i++) {
             for (int j=0; j<=i; j++) {
